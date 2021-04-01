d77f0ff98d5325234fba7618507e2e61ddad4363
grudgey <grudgey@grudgeys-iMac.local>
2021-04-01 10:56:31 +0100

    inital chess react commit

---
+++ src/components/Board.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import React from 'react';
+
+const {boardState, setBoardState} = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+
+export default function Board() {
+    return <div>Board</div>
+}
